1a29205af3c112212198e4127d76602d001c124f
pythonuzbek <nusratullo3304@gmail.com>
2023-07-11 17:16:03 +0500

    solve database example

---
+++ database/bank-account-summary-ii.sql
@@ -0,0 +1,2 @@
+select name, sum(amount) as balance from Users join Transactions on Users.account = Transactions.account group by Transactions.account
+having balance > 10000
---
+++ database/replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,1 @@
+select EmployeeUNI.unique_id, name from Employees left join EmployeeUNI on Employees.id = EmployeeUNI.id;
